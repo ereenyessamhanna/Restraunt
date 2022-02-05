d9b1470c924ca76189ae23ec9a2e36aaacfc92c6
Ereeny Hanna <ereenyessam@hotmail.com>
2022-02-05 13:27:38 +0200

    get resturants from json

--- app/utilities/utilities.ts
+++ app/utilities/utilities.ts
@@ -1,10 +1,15 @@
-import { FilterItemType } from '../components';
+import { FilterItemType, RestaurantType } from '../components';
 import { brands } from './data.json';
 
-export const getTags = () => {
+export const getTags = (): Array<FilterItemType> => {
     var tags: Array<FilterItemType> = []
     brands.forEach(element => {
         tags = tags.concat(element.tags)
     });
     return [...new Map(tags.map(element => [element.name, element])).values()]
+}
+
+export const getRestaurants = (): Array<RestaurantType> => {
+    var restaurant : Array<RestaurantType> = brands;
+    return restaurant;
 }
